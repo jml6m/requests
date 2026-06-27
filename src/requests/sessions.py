d4423389bb37b8fb71535c3b4eb79c5d016f23f2
OS Agent Orchestrator <os-agent@local>
2026-06-27 19:20:11 -0400

    Agent fix for issue #3829

--- src/requests/sessions.py
+++ src/requests/sessions.py
@@ -852,7 +852,7 @@ class Session(SessionRedirectMixin):
 
             # Look for requests environment configuration
             # and be compatible with cURL.
-            if verify is True or verify is None:
+            if verify is True or (verify is None and self.verify is not False):
                 verify = (
                     os.environ.get("REQUESTS_CA_BUNDLE")
                     or os.environ.get("CURL_CA_BUNDLE")
